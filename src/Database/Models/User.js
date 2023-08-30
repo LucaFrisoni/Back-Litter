6dde94912849b42bf134f067176f17caee4262f6
LucaFrisoni <frisoniluca1@gmail.com>
2023-08-29 23:06:02 -0300

    feat:Add Post in User

--- src/Database/Models/User.js
+++ src/Database/Models/User.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.pre("findById", function () {
+  this.populate("posts");
+});
+
 
 const User = mongoose.model("User", userSchema);
 
