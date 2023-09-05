1d9e145a60829f7cc7823b1d2ec76790742fa76b
LucaFrisoni <frisoniluca1@gmail.com>
2023-09-04 21:44:32 -0300

    feat: Cloudinary Implemented

--- src/Controllers/edit/handlerEdit.js
+++ src/Controllers/edit/handlerEdit.js
@@ -1,39 +1,142 @@
 const User = require("../../Database/Models/User");
+const { cloudinary } = require("../../cloudinary");
 
- const editUser = async (req, res) => {
+const editUser = async (req, res) => {
   const { email, username, ...updateFields } = req.body;
 
   try {
-    if (username) {
-      const lookSamePerson = await User.findOne({ username, email });
+    // Verificar si el usuario ya existe por username y email
+    const lookSamePerson = await User.findOne({ username, email });
 
-      if (lookSamePerson) {
-        const updatedUser = await User.findOneAndUpdate(
-          { email }, // Buscar por el email proporcionado
-          { ...updateFields }, // Actualizar los campos proporcionados
-          { new: true }
+    if (lookSamePerson) {
+      // Comprueba si se proporcionó una nueva imagen de perfil y cárgala en Cloudinary
+      if (updateFields.profileImage) {
+        const profileCloudinary = await cloudinary.uploader.upload(
+          updateFields.profileImage,
+          {
+            transformation: {
+              crop: "thumb",
+              gravity: "auto",
+              fetch_format: "auto",
+              quality: "auto",
+            },
+            folder: "TwitterClone",
+          }
         );
-
-        res.status(200).json(updatedUser);
-      } else {
-        const lookUniqueUserName = await User.findOne({ username });
-
-        if (lookUniqueUserName) {
-          res.status(400).json("Username Already Exist");
+        console.log("Cloudy =>", profileCloudinary.secure_url);
+        console.log("Actual user =>", lookSamePerson.profileImage);
+        if (profileCloudinary.secure_url != lookSamePerson.profileImage) {
+          updateFields.profileImage = profileCloudinary.secure_url;
+        } else {
+          const deleteCloudRepeat1 = await cloudinary.uploader.destroy(
+            profileCloudinary.public_id,
+            { invalidate: true }
+          );
         }
+        // Actualiza el campo de imagen de perfil con el enlace de Cloudinary
+      }
 
-        const updatedUser = await User.findOneAndUpdate(
-          { email }, // Buscar por el email proporcionado
-          { username, ...updateFields }, // Actualizar los campos proporcionados
-          { new: true }
+      // Comprueba si se proporcionó una nueva imagen de portada y cárgala en Cloudinary
+      if (updateFields.coverImage) {
+        const coverCloudinary = await cloudinary.uploader.upload(
+          updateFields.coverImage,
+          {
+            transformation: {
+              crop: "thumb",
+              gravity: "auto",
+              fetch_format: "auto",
+              quality: "auto",
+            },
+            folder: "TwitterClone",
+          }
+        );
+        if (coverCloudinary.secure_url != lookSamePerson.coverImage) {
+          updateFields.coverImage = coverCloudinary.secure_url;
+        } else {
+          const deleteCloudRepeat2 = await cloudinary.uploader.destroy(
+            coverCloudinary.public_id,
+            { invalidate: true }
+          );
+        }
+        // Actualiza el campo de imagen de portada con el enlace de Cloudinary
+      }
+
+      // Actualiza el usuario con los campos actualizados, incluyendo las imágenes
+      const updatedUser = await User.findOneAndUpdate({ email }, updateFields, {
+        new: true,
+      });
+
+      return res.status(200).json(updatedUser);
+    } else {
+      // El usuario no existe, verifica si el nombre de usuario ya está en uso
+      const lookUniqueUserName = await User.findOne({ username });
+
+      if (lookUniqueUserName) {
+        return res.status(400).json("Username Already Exist");
+      }
+
+      // Comprueba si se proporcionó una nueva imagen de perfil y cárgala en Cloudinary
+      if (updateFields.profileImage) {
+        const profileCloudinary = await cloudinary.uploader.upload(
+          updateFields.profileImage,
+          {
+            transformation: {
+              crop: "thumb",
+              gravity: "auto",
+              fetch_format: "auto",
+              quality: "auto",
+            },
+            folder: "TwitterClone",
+          }
+        );
+        if (profileCloudinary.secure_url != lookSamePerson.profileImage) {
+          updateFields.profileImage = profileCloudinary.secure_url;
+        } else {
+          const deleteCloudRepeat3 = await cloudinary.uploader.destroy(
+            profileCloudinary.public_id,
+            { invalidate: true }
+          );
+        }
+        // Actualiza el campo de imagen de perfil con el enlace de Cloudinary
+      }
+
+      // Comprueba si se proporcionó una nueva imagen de portada y cárgala en Cloudinary
+      if (updateFields.coverImage) {
+        const coverCloudinary = await cloudinary.uploader.upload(
+          updateFields.coverImage,
+          {
+            transformation: {
+              crop: "thumb",
+              gravity: "auto",
+              fetch_format: "auto",
+              quality: "auto",
+            },
+            folder: "TwitterClone",
+          }
         );
 
-        res.status(200).json(updatedUser);
+        // Actualiza el campo de imagen de portada con el enlace de Cloudinary
+        if (coverCloudinary.secure_url != lookSamePerson.coverImage) {
+          updateFields.coverImage = coverCloudinary.secure_url;
+        } else {
+          const deleteCloudRepeat2 = await cloudinary.uploader.destroy(
+            coverCloudinary.public_id,
+            { invalidate: true }
+          );
+        }
       }
+
+      // Crea un nuevo usuario con los campos proporcionados y las imágenes
+      const updatedUser = await User.findOneAndUpdate({ email }, updateFields, {
+        new: true,
+      });
+
+      return res.status(200).json(updatedUser);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error updating user" });
+    return res.status(500).json({ error: "Error updating user" });
   }
 };
-module.exports = editUser
+
+module.exports = editUser;
