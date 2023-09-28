bb60885698d60e801181406fea50322f33b8793c
LucaFrisoni <frisoniluca1@gmail.com>
2023-09-28 15:43:12 -0300

    fawa

--- src/Controllers/retweet/handler.js
+++ src/Controllers/retweet/handler.js
@@ -14,6 +14,7 @@ const retweetPost = async (req, res) => {
     }
 
     post.rt.active = true;
+    post.rt.userRetweet = userRetweet;
     post.retweets.push(userRetweet);
     await post.save();
 
